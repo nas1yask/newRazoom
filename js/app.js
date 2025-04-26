4b78816d986c8d82099aa02f01db42b0f5090f19
Nastya <nastenaastana@gmail.com>
2025-04-26 12:51:28 +0500

    just

--- js/app.js
+++ js/app.js
@@ -9,6 +9,16 @@ $(document).ready(function(){
         speed: 1000
     });
 
+    $('.swiper-wrapperFB').slick({
+      autoplay: true,
+        autoplaySpeed: 2500,
+        fade: true,
+        pauseOnHover: false
+  });
+
+
+
+    
     // TYPED JS 
     var typed = new Typed('.services_type', {
         strings: ['по изготовлению рекламы,', 'полиграфической продукции', 'и комплексному сопровождению мероприятий'],
@@ -27,35 +37,31 @@ $(document).ready(function(){
       }).eq(0).addClass("active");
 
 
-    //   SWIPER
-
+      // SWIPER
       
-    //   const swiper = new Swiper(".mySwiper", {
-    //     effect: "coverflow",
-    //     grabCursor: true,
-    //     centeredSlides: true,
-    //     slidesPerView: "auto",
-    //     coverflowEffect: {
-    //         rotate: 30,
-    //         stretch: 50,
-    //         depth: 300,
-    //         modifier: 1,
-    //         slideShadows: false,
-    //     },
-    //     loop: true,
-    //     pagination: {
-    //         el: ".swiper-pagination",
-    //         clickable: true,
-    //     },
-    //     navigation: {
-    //         nextEl: ".swiper-button-next",
-    //         prevEl: ".swiper-button-prev",
-    //     },
-    //     autoplay: {
-    //         delay: 3000,
-    //         disableOnInteraction: false,
-    //     }
-    // });
+
+      const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 3,
+    loop: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
   
     //   MASK FOR TEL   
